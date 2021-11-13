2e29434b1e9ef53d6e4adf62b69c71ee93ef7761
Shahzad Aslam <sa@digishares.io>
2021-11-13 12:15:07 +0500

    changes in ERC1404 token to include whitelist authority control

--- contracts/ERC1404TokenMinKYC.sol
+++ contracts/ERC1404TokenMinKYC.sol
@@ -5,20 +5,31 @@ pragma solidity ^0.8.0;
 import "./IERC1404.sol";
 import "./SafeMathMin.sol";
 
-contract ERC1404TokenMin is IERC1404 {
+contract ERC1404TokenMinKYC is IERC1404 {
 
     using SafeMathMin for uint256;
 	
+	// Set buy and sell restrictions on investors.  
+	// date is Linux Epoch datetime
+	// Both date must be less than current date time to allow the respective operation. Like to get tokens from others, receiver's buy restriction
+	// must be less than current date time. 
+	// 0 means investor is not allowed to buy or sell his token.  0 indicates buyer or seller is not whitelisted. 
+	// this condition is checked in detectTransferRestriction
     mapping (address => uint256) private _buyRestriction;  
 	mapping (address => uint256) private _sellRestriction;	
+	
 	mapping (address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
 	address private _owner;
+	
+	// These addresses can control addresses that can manage whitelisting of investor or in otherwords can call modifyKYCData
+    mapping (address => bool) private _whitelistControlAuthority;  	
+	
 
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
     event Transfer(address indexed from, address indexed to, uint tokens);
 
-	uint256 public constant decimals = 18;
+	uint256 public decimals = 18;
     uint256 public totalSupply;
     string public name;
     string public symbol;
@@ -27,20 +38,23 @@ contract ERC1404TokenMin is IERC1404 {
 	// if allowedInvestors = 0 then there is no limit of investors 
 	uint256 public currentTotalInvestors = 0;		
 	uint256 public allowedInvestors = 0;
-	
-	// If Trnasfers allowed or not
+
+	// If overall Trnasfers allowed or not     1=Normal Transfers     2=Stop all Transfers 
 	uint256 public isTradingAllowed = 1;
 	
-	string private constant AddressZeroMessage = "Address Zero Error";
-	string private constant AmountExceedBalance = "Amount Exceed Balance";	
+	string private constant AddressZeroMessage = "Zero Address";
+	string private constant AmountExceedBalance = "Amount > Balance";	
 	string private constant NotWhitelisted = "Not Whitelisted";
 
-	constructor(uint256 _initialSupply, string memory _name,  string memory _symbol, uint256 _allowedInvestors ) {
+
+	constructor(uint256 _initialSupply, string memory _name,  string memory _symbol, uint256 _allowedInvestors, uint256 _decimals ) {
 		name = _name;
         symbol = _symbol;
+		
+		decimals = _decimals;
 
-		_buyRestriction[msg.sender] = 4000000000;
-		_sellRestriction[msg.sender] = 4000000000;
+		_buyRestriction[msg.sender] = 1;
+		_sellRestriction[msg.sender] = 1;
 		_owner = msg.sender;
 
 		allowedInvestors = _allowedInvestors;
@@ -48,26 +62,35 @@ contract ERC1404TokenMin is IERC1404 {
 		// Minting tokens for initial supply
         totalSupply = _initialSupply;
         _balances[msg.sender] = totalSupply;
+		
+		// add message sender to whitelist authority list
+		_whitelistControlAuthority[msg.sender] = true;
 				
 		emit Transfer(address(0), msg.sender, totalSupply);
 	}
 
 
-	
+	// _allowedInvestors = 0    No limit on number of investors        
+	// _allowedInvestors > 0 only X number of investors can have positive balance 
     function resetAllowedInvestors(uint256 _allowedInvestors) 
 	public 
 	onlyOwner {
 		 allowedInvestors = _allowedInvestors;
     }
 
+
+	// 1 = Tading Allowed
+	// 2 = Stop all Transfers 
     function setTradingEnabled(uint256 _isTradingAllowed) 
 	public 
 	onlyOwner {
 		 isTradingAllowed = _isTradingAllowed;
     }
  
-
-    function getOwner() 
+ 
+	//-----------------------------------------------------------------------
+	// Get or set current owner of this smart contract
+    function owner() 
 	public 
 	view 
 	returns (address) {
@@ -83,13 +106,43 @@ contract ERC1404TokenMin is IERC1404 {
         require(newOwner != address(0), AddressZeroMessage);
 		_owner = newOwner;
     }
-
+	//-----------------------------------------------------------------------
+	
+  
   
 
+	  
+
+
+	  
+ 
+	//-----------------------------------------------------------------------
+    // Manage whitelist autority and KYC status
+	
+	function setWhitelistAuthorityStatus(address user)
+	public 
+	onlyOwner {
+		_whitelistControlAuthority[user] = true;
+	}
+	function removeWhitelistAuthorityStatus(address user)
+	public 
+	onlyOwner {
+		delete _whitelistControlAuthority[user];
+	}	
+	function getWhitelistAuthorityStatus(address user)
+	public 
+	view
+	returns (bool) {
+		 return _whitelistControlAuthority[user];
+	}	
+	
+	
+  	  // Set buy and sell restrictions on investors 
 	  function modifyKYCData (address user, uint256 buyRestriction, uint256 sellRestriction) 
 	  public 
-	  onlyOwner 
 	  { 
+	  	    require(_whitelistControlAuthority[msg.sender] == true, "Not Whitelist Authority");
+			
 		   _buyRestriction[user] = buyRestriction;
 		   _sellRestriction[user] = sellRestriction;
 	  }
@@ -100,18 +153,21 @@ contract ERC1404TokenMin is IERC1404 {
 	  returns (uint256, uint256 ) {
 		   return (_buyRestriction[user] , _sellRestriction[user] );
 	  }
+	//-----------------------------------------------------------------------
 
 
 
+
+
+	//-----------------------------------------------------------------------
+	// These are ERC1404 interface implementations 
+	
     modifier notRestricted (address from, address to, uint256 value) {
         uint8 restrictionCode = detectTransferRestriction(from, to, value);
         require(restrictionCode == 1, messageForTransferRestriction(restrictionCode));
         _;
     }
-
-
-
-	// These are ERC1404 interface implementations 
+	
     function detectTransferRestriction (address _from, address _to, uint256 value) 
 	override
 	public 
@@ -120,10 +176,11 @@ contract ERC1404TokenMin is IERC1404 {
     {
 	      // check if trading is allowed 
 		  require(isTradingAllowed == 1, "Transfer not allowed"); 		  			  
-		  require( _sellRestriction[_from] > block.timestamp, NotWhitelisted  );
-		  require( _buyRestriction[_to] > block.timestamp, NotWhitelisted  );
+		  require(  _sellRestriction[_from] != 0 && _sellRestriction[_from] < block.timestamp, NotWhitelisted  );
+		  require(  _buyRestriction[_to] != 0 && _buyRestriction[_to] < block.timestamp, NotWhitelisted  );
 
 			// Following conditions make sure if number of token holders are within limit if enabled 
+			// 0 means no restriction
 			if(allowedInvestors == 0)
 				return 1;
 			else {
@@ -148,6 +205,9 @@ contract ERC1404TokenMin is IERC1404 {
          else 
             message = NotWhitelisted;
     }
+	//-----------------------------------------------------------------------
+
+
 
 
 
@@ -169,7 +229,7 @@ contract ERC1404TokenMin is IERC1404 {
 	{
         require(recipient != address(0), AddressZeroMessage);
         require(_balances[msg.sender] >= amount, AmountExceedBalance);
-				
+
 		transferSharesBetweenInvestors ( msg.sender, recipient, amount );
 		
         emit Transfer(msg.sender, recipient, amount);
@@ -210,7 +270,7 @@ contract ERC1404TokenMin is IERC1404 {
 
 		transferSharesBetweenInvestors ( owner, recipient, amount );
         _allowances[owner][msg.sender] = _allowances[owner][msg.sender].sub(amount);
-		
+
         emit Transfer(owner, recipient, amount);	
     }
 
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,12 +1,12 @@
 //const ERC1404 = artifacts.require("ERC1404Token");
 //var ERC20 = artifacts.require("ERC20Token");
 
-var ERC1404TokenMin = artifacts.require("ERC1404TokenMin");
+var ERC1404TokenMinKYC = artifacts.require("ERC1404TokenMinKYC");
 
 module.exports = function (deployer) {	
 	 var _tokenToMintAddress = "0xAD3DF0f1c421002B8Eff81288146AF9bC692d13d";
-	 let _initialSupply = "1000000000000000000000000";
-	
+	 let _initialSupply = "100000000000000000000000";
+	//
 	
 	 //var _name = "E1404"
 	 //deployer.deploy(ERC1404, _tokenToMintAddress, _initialSupply, _name);
@@ -15,12 +15,7 @@ module.exports = function (deployer) {
 	 //deployer.deploy(ERC20, _tokenToMintAddress, _initialSupply, _name);	
 	
 		
-	 var _name = "ER14"
-	 deployer.deploy(ERC1404TokenMin, _initialSupply, _name, _name, 5);
-	
+	 var _name = "EK14"
+	 deployer.deploy(ERC1404TokenMinKYC, _initialSupply, _name, _name, 5, 18);
 	
 };
-
-
-
-
