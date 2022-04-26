3bb81beb290e684bf387d192ac3a83ab988d0019
Shahzad Aslam <sa@digishares.io>
2022-04-26 13:45:55 +0500

    some changes in deployment

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,6 @@
 //const ERC1404 = artifacts.require("ERC1404Token");
-//var ERC20 = artifacts.require("ERC20Token");
-
-var ERC1404TokenMinKYC = artifacts.require("ERC1404TokenMinKYC");
+var ERC20 = artifacts.require("ERC20Token");
+//var ERC1404TokenMinKYC = artifacts.require("ERC1404TokenMinKYC");
 
 module.exports = function (deployer) {	
 	 var _tokenToMintAddress = "0xAD3DF0f1c421002B8Eff81288146AF9bC692d13d";
@@ -11,11 +10,11 @@ module.exports = function (deployer) {
 	 //var _name = "E1404"
 	 //deployer.deploy(ERC1404, _tokenToMintAddress, _initialSupply, _name);
 	
-	 //_name = "E20"
-	 //deployer.deploy(ERC20, _tokenToMintAddress, _initialSupply, _name);	
+	 _name = "USDC"
+	 deployer.deploy(ERC20, _tokenToMintAddress, _initialSupply, _name);	
 	
 		
-	 var _name = "EK14"
-	 deployer.deploy(ERC1404TokenMinKYC, _initialSupply, _name, _name, 5, 18, "ShareCertificate", "CompanyHomepage", "CompanyLegalDocs");
+	 //var _name = "EK14"
+	 //deployer.deploy(ERC1404TokenMinKYC, _initialSupply, _name, _name, 5, 18, "ShareCertificate", "CompanyHomepage", "CompanyLegalDocs");
 
 };
